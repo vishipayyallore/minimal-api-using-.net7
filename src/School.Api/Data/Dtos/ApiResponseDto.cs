e011ec5f271da54d99d29c61084e490e39440082
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-07-24 20:44:49 +0530

    Swamy/23jul session1 work (#5)
    * Initial Version
    
    * Initial Version
    
    * Documentation
    
    * Changes
    
    * Converted into Minimal API
    
    * Update launchSettings.json
    
    * Settings to work with VS Code
    
    * Update launch.json
    
    * Create build-and-test.yml
    
    * Update build-and-test.yml
    
    * Update build-and-test.yml
    
    * Update build-and-test.yml
    
    * Update build-and-test.yml
    
    * Update build-and-test.yml
    
    * Update build-and-test.yml
    
    * Update build-and-test.yml
    
    * Update build-and-test.yml
    
    * Update build-and-test.yml
    
    * Update build-and-test.yml
    
    * Update build-and-test.yml
    
    * Update build-and-test.yml
    
    * Update build-and-test.yml
    
    * Update build-and-test.yml
    
    * Update build-and-test.yml
    
    * Few Changes
    
    * Update build-and-test.yml
    
    * Update build-and-test.yml
    
    * Code
    
    * 4 Endpoints
    
    * Hello World Endpoints
    
    * Endpoints

---
+++ src/School.Api/Data/Dtos/ApiResponseDto.cs
@@ -0,0 +1,22 @@
+﻿namespace School.API.Data.Dtos;
+
+public record ApiResponseDto<T>
+{
+    public bool Success { get; set; }
+
+    public string? Message { get; set; }
+
+    public DateTimeOffset DateRequested => DateTimeOffset.UtcNow;
+
+    public T? Data { get; set; }
+
+    public static ApiResponseDto<T> Create(T? data = default, string message = "Success", bool success = true)
+    {
+        return new()
+        {
+            Success = success,
+            Message = message,
+            Data = data
+        };
+    }
+}
---
+++ src/School.Api/Endpoints/HelloWorldEndpoints.cs
@@ -0,0 +1,32 @@
+﻿using School.API.Business;
+using School.API.Data.Dtos;
+using static School.API.ApplicationCore.Common.Constants;
+
+namespace School.API.Endpoints;
+
+public static class HelloWorldEndpoints
+{
+
+    public static void MapHelloWorldEndpoints(this IEndpointRouteBuilder routes)
+    {
+        _ = routes
+            .MapGet(HelloWorldRoutes.Root, () => "Minimal API. Please visit /swagger for full documentation")
+            .WithTags("Hello World")
+            .WithName("Get HelloWorldString");
+
+        _ = routes.MapGet(HelloWorldRoutes.HelloWorld, () =>
+        {
+            return ApiResponseDto<string>.Create("Hello Minimal API World from /hw !!");
+        }).WithTags("Hello World")
+          .WithName("Get ApiResponseDto String");
+
+        _ = routes.MapGet(HelloWorldRoutes.Api, DefaultResponseBusiness.SendDefaultApiEndpointOutput)
+            .WithTags("Hello World")
+            .WithName("Get Api");
+
+        _ = routes.MapGet(HelloWorldRoutes.ApiV1, () => DefaultResponseBusiness.SendDefaultApiEndpointV1Output())
+            .WithTags("Hello World")
+            .WithName("Get Api V1");
+    }
+
+}
