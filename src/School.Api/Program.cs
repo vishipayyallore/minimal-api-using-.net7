a385e71cf397aa6560ea8155b4a659d3b76d1cba
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-07-24 20:32:43 +0530

    Hello World Endpoints

--- src/School.Api/Program.cs
+++ src/School.Api/Program.cs
@@ -1,5 +1,6 @@
 using School.API.Business;
 using School.API.Data.Dtos;
+using School.API.Endpoints;
 using static School.API.ApplicationCore.Common.Constants;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -20,16 +21,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.MapGet(HelloWorldRoutes.Root, () => "Hello Minimal API World from Root !!");
-
-app.MapGet(HelloWorldRoutes.HelloWorld, () =>
-{
-    return ApiResponseDto<string>.Create("Hello Minimal API World from /hw !!");
-});
-
-app.MapGet(HelloWorldRoutes.Api, DefaultResponseBusiness.SendDefaultApiEndpointOutput);
-
-app.MapGet(HelloWorldRoutes.ApiV1, () => DefaultResponseBusiness.SendDefaultApiEndpointV1Output());
+app.MapHelloWorldEndpoints();
 
 app.Run();
-
