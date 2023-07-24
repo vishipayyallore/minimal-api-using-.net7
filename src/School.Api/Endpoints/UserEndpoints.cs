4c18edd697eddeb5af7d3bbc058dd9acb80610bf
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-07-24 21:08:19 +0530

    Swamy/23jul session1 work (#6)
    * Initial Version
    
    * Initial Version
    
    * Documentation
    
    * Changes
    
    * Converted into Minimal API
    
    * Update launchSettings.json
    
    * Settings to work with VS Code
    
    * Update launch.json
    
    * Create build-and-test.yml
    
    * Update build-and-test.yml
    
    * Update build-and-test.yml
    
    * Update build-and-test.yml
    
    * Update build-and-test.yml
    
    * Update build-and-test.yml
    
    * Update build-and-test.yml
    
    * Update build-and-test.yml
    
    * Update build-and-test.yml
    
    * Update build-and-test.yml
    
    * Update build-and-test.yml
    
    * Update build-and-test.yml
    
    * Update build-and-test.yml
    
    * Update build-and-test.yml
    
    * Update build-and-test.yml
    
    * Update build-and-test.yml
    
    * Few Changes
    
    * Update build-and-test.yml
    
    * Update build-and-test.yml
    
    * Code
    
    * 4 Endpoints
    
    * Hello World Endpoints
    
    * Endpoints
    
    * Added User Endpoints

---
+++ src/School.Api/Endpoints/UserEndpoints.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using School.API.Data.Dtos;
+using static School.API.ApplicationCore.Common.Constants;
+
+namespace School.API.Endpoints;
+
+public static class UserEndpoints
+{
+
+    public static void MapUserEndpoints(this IEndpointRouteBuilder routes)
+    {
+        var group = routes.MapGroup(UsersRoutes.Prefix).WithTags(nameof(PersonDto));
+
+        _ = group.MapGet(UsersRoutes.ActionById, ([FromRoute] string id, [FromQuery] string name) =>
+        {
+            return ApiResponseDto<dynamic>.Create(new
+            {
+                UserId = id,
+                Message = $"Hello {name}, Welcome to Minimal API World !!"
+            });
+        }).AllowAnonymous()
+          .WithName("GetUserById")
+          .Produces<ApiResponseDto<dynamic>>(StatusCodes.Status200OK)
+          .ProducesProblem(StatusCodes.Status500InternalServerError)
+          .WithOpenApi();
+
+        _ = group.MapPost(UsersRoutes.Root, ([FromBody] PersonDto person) =>
+        {
+            return ApiResponseDto<dynamic>.Create(new
+            {
+                UserId = person.Id,
+                UserName = person.Name,
+            });
+        }).WithName("AddNewUser")
+          .Produces<ApiResponseDto<dynamic>>(StatusCodes.Status200OK)
+          .ProducesProblem(StatusCodes.Status500InternalServerError)
+          .WithOpenApi();
+
+    }
+
+
+}
